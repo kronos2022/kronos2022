7cbd9a861f0cae6a594db9f95666326b7ec70924
Aishwarya Bhat <aishbhat22@gmail.com>
2022-04-01 22:39:05 +0530

    Changed mapping

--- src/main/java/com/documentprocessing/controller/ClassificationController.java
+++ src/main/java/com/documentprocessing/controller/ClassificationController.java
@@ -3,6 +3,7 @@ package com.documentprocessing.controller;
 import com.documentprocessing.service.ClassificationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ class ClassificationController {
     @Autowired
     ClassificationService classificationService;
 
-    @PostMapping(value = "/test")
+    @GetMapping(value = "/test")
     private String testApplication() {
         return "Spring boot application running";
     }
