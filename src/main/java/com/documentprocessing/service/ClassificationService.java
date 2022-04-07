07ef347bb1beabb469ca8dbc2e1d3714c3a91454
Komal H <travelforlife92@gmail.com>
2022-04-07 20:21:35 +0530

    Adding OCR code

--- src/main/java/com/documentprocessing/service/ClassificationService.java
+++ src/main/java/com/documentprocessing/service/ClassificationService.java
@@ -1,17 +1,23 @@
 package com.documentprocessing.service;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.automl.v1.*;
 import com.google.cloud.documentai.v1.Document;
 import com.google.cloud.documentai.v1.*;
+import com.google.common.collect.Lists;
 import com.google.protobuf.ByteString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -21,57 +27,32 @@ public class ClassificationService {
     public void classifyDocument(String projectId, String location, String processorId, String filePath, String modelId) throws IOException {
 
         try (DocumentProcessorServiceClient client = DocumentProcessorServiceClient.create()) {
-
             String name =
                     String.format("projects/%s/locations/%s/processors/%s", projectId, location, processorId);
 
-            byte[] pdfFileData = Files.readAllBytes(Paths.get(filePath));
+            // Read the file.
+            byte[] imageFileData = Files.readAllBytes(Paths.get(filePath));
 
-            ByteString content = ByteString.copyFrom(pdfFileData);
+            // Convert the image data to a Buffer and base64 encode it.
+            ByteString content = ByteString.copyFrom(imageFileData);
 
             RawDocument document =
                     RawDocument.newBuilder().setContent(content).setMimeType("application/pdf").build();
 
+            // Configure the process request.
             ProcessRequest request =
                     ProcessRequest.newBuilder().setName(name).setRawDocument(document).build();
 
+            // Recognizes text entities in the PDF document
             ProcessResponse result = client.processDocument(request);
             Document documentResponse = result.getDocument();
 
+            // Get all of the document text as one big string
             String text = documentResponse.getText();
 
-            log.info("Document Text" + text);
-            try (PredictionServiceClient predictionServiceClient = PredictionServiceClient.create()) {
-                ModelName modelName = ModelName.of(projectId, "us-central1", modelId);
-                TextSnippet textSnippet =
-                        TextSnippet.newBuilder().setContent(text)
-                                .setMimeType("text/plain")
-                                .build();
-                ExamplePayload payload = ExamplePayload.newBuilder().setTextSnippet(textSnippet).build();
-                PredictRequest predictRequest = PredictRequest.newBuilder().setName(name.toString()).setPayload(payload).build();
-                PredictResponse response = predictionServiceClient.predict(predictRequest);
-
-                HashMap<String, Float> labelMap = new HashMap<>();
-                String predictedLabel = "NONE";
-                for (AnnotationPayload annotationPayload : response.getPayloadList()) {
-                    labelMap.put(annotationPayload.getDisplayName(), annotationPayload.getClassification().getScore());
-                }
-                float maxScore = Collections.max(labelMap.values());
-                for (Map.Entry<String, Float> entry : labelMap.entrySet()) {
-                    if (entry.getValue() == maxScore) {
-                        predictedLabel = entry.getKey();
-                    }
-                }
-                log.info(("Predicted Label: " + predictedLabel));
-                switch (predictedLabel) {
-                    case "Case1":
-                        log.info("In Case1");
-                        break;
-                    case "Case2":
-                        log.info("In Case2");
-                        break;
-                }
-            }
+            // Read the text recognition output from the processor
+            System.out.println("Full text: "+text);
         }
     }
 }
+
