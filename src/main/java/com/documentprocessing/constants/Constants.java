032f60c9c9d7a0d955096203ca0a7103f78095f3
Rahul Nair <nair.rahul207@gmail.com>
2022-04-12 21:04:38 +0530

    Rename model

--- src/main/java/com/documentprocessing/constants/Constants.java
+++ src/main/java/com/documentprocessing/constants/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static final String LOCATION_CENTRAL_US ="us-central1";
     public static final String OCR_PROCESSOR_ID = "60c96e1afcf82c4a";
     public static final String CLASSIFY_MODEL_ID = "TCN3877526711398563840";
-    public static final String INSURANCE_MODEL_ID = "TEN8598636127022219264";
+    public static final String INSURANCE_MODEL_ID = "TEN6629718664931180544";
     public static final String INSURANCE_MODEL_NAME = "Insurance_documents";
     public static final String FCA_MODEL_NAME = "FCA";
 }
